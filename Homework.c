08d273947940639ebcdf95b30ab526513c46d7f5
JeongMinJin <31675724+JeongMinJin@users.noreply.github.com>
2017-09-06 11:06:59 +0900

    Homework.c program
    Program which prints out my profile including student id number, mt name, mobile phone number and GitHub address

---
+++ Homework.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+printf("My Student id number is 2017301068 \n");
+printf("My name is Jeong Min Jin \n");
+printf("My Mobile phone number is 010-4027-5390 \n");
+printf("My GitHub address is https://github.com/JeongMinJin' \n");
+return 0;
+}
